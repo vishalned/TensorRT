ac84c3ae405e190a4012e96d291fd0e554302016
root <root@ip-172-31-6-2.ap-south-1.compute.internal>
2021-07-08 08:31:37 +0000

    changed convert_to_onnx

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -1,11 +1,20 @@
 import keras
 import tensorflow as tf
 from keras2onnx import convert_keras
+import tensorflow as tf
+import onnx
+import tf2onnx.convert
 
-def keras_to_onnx(model, output_filename):
-   onnx = convert_keras(model, output_filename)
-   with open(output_filename, "wb") as f:
-       f.write(onnx.SerializeToString())
+model = tf.keras.models.load_model('./model.hdf5', compile = False)
 
-model = keras.models.load_model('./model.hdf5')
-keras_to_onnx(model, './model.onnx') 
+onnx_model, _ = tf2onnx.convert.from_keras(model, opset = 12)
+
+onnx.save(onnx_model, './model.onnx')
+
+#def keras_to_onnx(model, output_filename):
+#   onnx = convert_keras(model, output_filename)
+#   with open(output_filename, "wb") as f:
+#       f.write(onnx.SerializeToString())
+
+#model = keras.models.load_model('./model.hdf5', compile = False)
+#keras_to_onnx(model, './model.onnx') 
