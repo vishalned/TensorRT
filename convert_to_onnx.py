334e65fee1d604b0584a4e8c43533fac3e6830f0
root <root@ip-172-31-6-2.ap-south-1.compute.internal>
2021-07-12 17:32:27 +0000

    changed inference

--- convert_to_onnx.py
+++ convert_to_onnx.py
@@ -4,17 +4,67 @@ from keras2onnx import convert_keras
 import tensorflow as tf
 import onnx
 import tf2onnx.convert
+from keras.optimizers import Adam
+from tensorflow.keras import backend as K
 
-model = tf.keras.models.load_model('./model.hdf5')
+'''
+def dice_soft(y_true, y_pred, smooth=0.00001):
+    # Identify axis
+    axis = identify_axis(y_true.get_shape())
+
+    # Calculate required variables
+    intersection = y_true * y_pred
+    intersection = K.sum(intersection, axis=axis)
+    y_true = K.sum(y_true, axis=axis)
+    y_pred = K.sum(y_pred, axis=axis)
+
+    # Calculate Soft Dice Similarity Coefficient
+    dice = ((2 * intersection) + smooth) / (y_true + y_pred + smooth)
+
+    # Obtain mean of Dice & return result score
+    dice = K.mean(dice)
+    return dice
+
+def tversky_loss(y_true, y_pred, smooth=0.000001):
+    # Define alpha and beta
+    alpha = 0.5
+    beta  = 0.5
+    # Calculate Tversky for each class
+    axis = identify_axis(y_true.get_shape())
+    tp = K.sum(y_true * y_pred, axis=axis)
+    fn = K.sum(y_true * (1-y_pred), axis=axis)
+    fp = K.sum((1-y_true) * y_pred, axis=axis)
+    tversky_class = (tp + smooth)/(tp + alpha*fn + beta*fp + smooth)
+    # Sum up classes to one score
+    tversky = K.sum(tversky_class, axis=[-1])
+    # Identify number of classes
+    n = K.cast(K.shape(y_true)[-1], 'float32')
+    # Return Tversky
+    return n-tversky
+
+def identify_axis(shape):
+    # Three dimensional
+    if len(shape) == 5 : return [1,2,3]
+    # Two dimensional
+    elif len(shape) == 4 : return [1,2]
+    # Exception - Unknown
+    else : raise ValueError('Metric: Shape of tensor is neither 2D or 3D.')
+'''
+model = tf.keras.models.load_model('./reshaped_model.hdf5', compile = False)
 
 onnx_model, _ = tf2onnx.convert.from_keras(model, opset = 12)
 
 onnx.save(onnx_model, './model.onnx')
+
 
 #def keras_to_onnx(model, output_filename):
 #   onnx = convert_keras(model, output_filename)
 #   with open(output_filename, "wb") as f:
 #       f.write(onnx.SerializeToString())
 
-#model = keras.models.load_model('./model.hdf5', compile = False)
+#model = keras.models.load_model('./model.hdf5', custom_objects = {'custom_loss':tversky_loss, 'custom_metrics':dice_soft})
+#model.compile(optimizer=Adam(lr=0.0001),
+#                           loss=tversky_loss, metrics=[dice_soft])
+
 #keras_to_onnx(model, './model.onnx') 
+
--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ import pycuda.driver as cuda
 import numpy as np
 import pycuda.autoinit 
 
+'''
 def allocate_buffers(engine, batch_size, data_type):
 
    """
@@ -22,10 +23,10 @@ def allocate_buffers(engine, batch_size, data_type):
    """
 
    #print(engine.get_binding_shape(0))
-   #print(trt.volume(engine.get_binding_shape(0)))
+   print('engine shape ',trt.volume(engine.get_binding_shape(0)))
    # Determine dimensions and create page-locked memory buffers (which won't be swapped to disk) to hold host inputs/outputs.
-   h_input_1 = cuda.pagelocked_empty(batch_size * trt.volume((160,160,80)), dtype=trt.nptype(data_type))
-   h_output = cuda.pagelocked_empty(batch_size * trt.volume((160, 160, 80, 4)), dtype=trt.nptype(data_type))
+   h_input_1 = cuda.pagelocked_empty(batch_size * trt.volume(engine.get_binding_shape(0)), dtype=trt.nptype(data_type))
+   h_output = cuda.pagelocked_empty(batch_size * trt.volume(engine.get_binding_shape(1)), dtype=trt.nptype(data_type))
    # Allocate device memory for inputs and outputs.
    d_input_1 = cuda.mem_alloc(h_input_1.nbytes)
 
@@ -59,8 +60,9 @@ def do_inference(engine, pics_1, h_input_1, d_input_1, h_output, d_output, strea
    """
    print('gpu var shape',h_input_1.shape)
    print('image shape in inference',pics_1.shape)
+   print(h_input_1.shape)
    load_images_to_buffer(pics_1, h_input_1)
-
+   print('shape of hinput ',h_input_1.shape)
    with engine.create_execution_context() as context:
        # Transfer input data to the GPU.
        cuda.memcpy_htod_async(d_input_1, h_input_1, stream)
@@ -68,12 +70,62 @@ def do_inference(engine, pics_1, h_input_1, d_input_1, h_output, d_output, strea
        # Run inference.
 
        context.profiler = trt.Profiler()
-       context.execute(batch_size=1, bindings=[int(d_input_1), int(d_output)])
+       cuda.memcpy_htod_async(d_input_1, h_input_1, stream)
+       context.execute_async(batch_size=1, bindings=[int(d_input_1), int(d_output)], stream_handle = stream.handle)
 
        # Transfer predictions back from the GPU.
        cuda.memcpy_dtoh_async(h_output, d_output, stream)
        # Synchronize the stream
        stream.synchronize()
        # Return the host output.
-       out = h_output.reshape((batch_size,-1, height, width, depth))
+       print(h_output.shape)
+       out = h_output.reshape((batch_size, height, width, depth, 4))
        return out 
+       
+'''
+class HostDeviceMem(object):
+    def __init__(self, host_mem, device_mem):
+        self.host = host_mem
+        self.device = device_mem
+
+    def __str__(self):
+        return "Host:\n" + str(self.host) + "\nDevice:\n" + str(self.device)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def allocate_buffers(engine):
+    inputs = []
+    outputs = []
+    bindings = []
+    stream = cuda.Stream()
+    for binding in engine:
+        size = trt.volume(engine.get_binding_shape(binding)) * engine.max_batch_size
+        dtype = trt.nptype(engine.get_binding_dtype(binding))
+        # Allocate host and device buffers
+        host_mem = cuda.pagelocked_empty(size, dtype)
+        device_mem = cuda.mem_alloc(host_mem.nbytes)
+        # Append the device buffer to device bindings.
+        bindings.append(int(device_mem))
+        # Append to the appropriate list.
+        if engine.binding_is_input(binding):
+            inputs.append(HostDeviceMem(host_mem, device_mem))
+        else:
+            outputs.append(HostDeviceMem(host_mem, device_mem))
+    return inputs, outputs, bindings, stream
+
+# This function is generalized for multiple inputs/outputs.
+# inputs and outputs are expected to be lists of HostDeviceMem objects.
+
+def do_inference(context, bindings, inputs, outputs, stream, batch_size=1):
+    # Transfer input data to the GPU.
+    [cuda.memcpy_htod_async(inp.device, inp.host, stream) for inp in inputs]
+    # Run inference.
+    context.execute_async(batch_size=batch_size, bindings=bindings, stream_handle=stream.handle)
+    # Transfer predictions back from the GPU.
+    [cuda.memcpy_dtoh_async(out.host, out.device, stream) for out in outputs]
+    # Synchronize the stream
+    stream.synchronize()
+    # Return only the host outputs.
+    return [out.host for out in outputs]
